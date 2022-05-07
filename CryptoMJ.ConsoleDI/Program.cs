868fe0464f3ad251220618918b278ab1c6c4b348
Marcos Junior <Mod.gluguer@gmail.com>
2022-05-07 14:16:37 -0300

    add methods to dependency injection

---
+++ CryptoMJ.ConsoleDI/Program.cs
@@ -0,0 +1,24 @@
+﻿using CryptoMJ;
+using Microsoft.Extensions.DependencyInjection;
+
+new ServiceCollection()
+    .AddCryptoMJ()
+    .AddScoped<Startup>()
+    .BuildServiceProvider()
+    .GetService<Startup>();
+
+public class Startup
+{
+    public Startup(ICrypto crypto)
+    {
+        string key = "key";
+        string textToEncrypt = "Marcos Antonio dos Santos Junior";
+        string encryptedText = crypto.Encrypt(textToEncrypt, key);
+        string decryptedText = crypto.Decrypt(encryptedText, key);
+
+        Console.WriteLine($"chave: {key}");
+        Console.WriteLine($"texto: {textToEncrypt}");
+        Console.WriteLine($"texto criptografado: {encryptedText}");
+        Console.WriteLine($"texto descriptografado: {decryptedText}");
+    }
+}
---
+++ CryptoMJ/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace CryptoMJ
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCryptoMJ(this IServiceCollection services, CryptoBuilder builder) => AddMethod(services, builder);
+        public static IServiceCollection AddCryptoMJ(this IServiceCollection services) => AddMethod(services, new CryptoBuilder());
+        private static IServiceCollection AddMethod(IServiceCollection services, CryptoBuilder builder)
+        {
+            services.AddSingleton<ICrypto, Crypto>(x => (Crypto)builder.Build());
+            return services;
+        }
+    }
+}
